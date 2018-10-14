f8f138bef27b366bb96e27aa6f348a6a122d1b46
rameshm <rameshm@wowtruck.in>
2018-10-14 11:17:46 +0530

    bug fixes

--- src/components/app.js
+++ src/components/app.js
@@ -7,7 +7,11 @@ class App extends Component {
     return (
       <View style={styles.rootContainer}>
         <View style={styles.displayContainer}>  
-          <Text style={styles.displayText}>{this.props.value}</Text>
+   <Text style={styles.displayText}>{this.props.prevalue!=0 ?this.props.prevalue : '' }
+    {(this.props.symbol !=null)? this.props.symbol :""} 
+    {(this.props.prevalue !=0 && this.props.symbol !=null 
+    && this.props.value ==0)?'' :this.props.value}  </Text> 
+     {/*<Text style={styles.displayText}>{this.props.value}</Text>*/}
         </View>
         <View style={styles.inputContainer}>
             {this.props.inputBtns()}
